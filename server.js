1dcbe32c75dcdf0a11c4b865b2c3453ad895c16a
hmc-tuna-f18 <amhuang@cs.hmc.edu>
2019-03-02 21:40:21 -0800

    123

--- server.js
+++ server.js
@@ -5,41 +5,7 @@
 var express = require('express');
 var app = express();
 
-// Authorization request
-const hash = window.location.hash
-  .substring(1)
-  .split('&')
-  .reduce(function (initial, item) {
-  if (item) {
-    var parts = item.split('=');
-    initial[parts[0]] = decodeURIComponent(parts[1]);
-  }
-  return initial;
-}, {});
-window.location.hash = '';
 
-// Set token
-let _token = hash.access_token;
-
-const authEndpoint = 'https://accounts.spotify.com/authorize';
-
-
-// Replace with your app's client ID, redirect URI and desired scopes
-const clientID = '6f90aae762534341aac911e306e7fc91';
-const redirectURI = 'http://localhost:8888'; //our browser
-const scopes = ['user-read-recently-played', 'user-library-read', 'playlist-read-private', 'playlist-read-collaborative', 'user-top-read', 'playlist-modify-public'];
-
-
-
-
-
-// If there is no token, redirect to Spotify authorization
-//if (!_token) {
-function authorize()
-{
-  return `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token`;
-}
-//}
 
 
 // SPOTIFY AUTHENTICATION
