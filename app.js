05ee32ac17e32d3695e4d188f52a2cb1ced683d7
hmc-tuna-f18 <amhuang@cs.hmc.edu>
2019-03-03 05:01:44 -0800

    123

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+// Authorization request
+const hash = window.location.hash
+  .substring(1)
+  .split('&')
+  .reduce(function (initial, item) {
+    if (item) {
+      var parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = '';
 // Set token
 let _token = hash.access_token;
 
